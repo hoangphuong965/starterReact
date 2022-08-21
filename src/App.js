7946dc40bfbc1210fd9d82f93434b69e026b4648
hoangphuong965 <hoangphuong965@gmail.com>
2022-08-22 01:44:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Switch, Route, Link} from "react-router-dom";
+
+
+const App = () => {
+   return (
+      <div>
+          App
+      </div>
+   );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ]
+    }
+
+}
